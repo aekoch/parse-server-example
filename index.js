e7d424efb9be89c0800ec535194314ade04b55f4
AaronEKoch <aaronkoch99@yahoo.com>
2018-06-13 17:51:37 -0500

    Parse Dashboard working, landing page init commit

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
+	res.sendfile(path.join(__dirname, 'public/home.html'))
 	res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
@@ -53,3 +54,5 @@ httpServer.listen(port, function() {
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
+
+Document.
